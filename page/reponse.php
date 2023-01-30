23b666c0279a1ec79d3c0a1015fe181053af17e2
Mucka <96072629+M-U-C-K-A@users.noreply.github.com>
2023-01-30 12:55:55 +0100

    Add files via upload

---
+++ page/reponse.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../media/reset.css">
+    <link rel="stylesheet" href="../media/main.css">
+    <link rel="stylesheet" href="../media/reponse.css">
+
+    <title>Eco responsable</title>
+</head>
+
+<body>
+    <header>
+        <ul>
+            <li><a href="#">Look Down</a></li>
+            <span>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="page/about.html">À propos</a></li>
+                <li><a href="page/agir.html">Agir</a></li>
+                <li><a href="page/eco.html">Eco résponsable</a></li>
+            </span>
+        </ul>
+    </header>
+    <!--Header-->
+
+    <main>
+
+        <section>
+            <h1>Prêt pour le <strong>defi</strong> ?</h1>
+            <blockquote>Voici quelques actions concrètes que vous pouvez mettre en place pour agir et soutenir la cause :</blockquote>
+        </section>
+
+        <section>
+           <div class="card">
+
+           <div class="arriane">
+            
+           </div>
+           </div> 
+    </main>
+
+    <footer>
+        <ul>
+            <li>look.down@gmail.com</li>
+            <li>Copyright 2023 - Tous droits réservés - Look Down</li>
+            <li><i class="fa-brands fa-square-instagram"></i><i class="fa-brands fa-square-twitter"></i><i
+                    class="fa-brands fa-square-facebook"></i></li>
+        </ul>
+    </footer>
+    <!--Footer-->
+</body>
+
+</html>
