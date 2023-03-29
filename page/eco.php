0003b8acb91ff9aa04755c64142aaf1d67430890
Mucka <96072629+M-U-C-K-A@users.noreply.github.com>
2023-03-29 13:45:50 +0200

    quizz

--- page/eco.php
+++ page/eco.php
@@ -12,7 +12,7 @@
     <title>Eco responsable</title>
 </head>
 
-<body>
+<body class="eco">
     <header>
         <ul>
             <li><a href="#">Look Down</a></li>
@@ -32,38 +32,51 @@
             <h1>Prêt pour le <strong>defi</strong> ?</h1>
             <blockquote>Voici quelques actions concrètes que vous pouvez mettre en place pour agir et soutenir la cause :</blockquote>
         </section>
-
-        <section>
-            <form action="" method="get">
+        <?php
+        session_start();
+        ?>
+        <form action="reponse.php" method="post">
             <?php
-            $question = ['question 1', 'question 2', 'question 3', 'question 4', 'question 5', 'question 6', 'question 7', 'question 8', 'question 9', 'question 10'];
-            $response = array(
-                array("réponse 1.1", "réponse 1.2", "réponse 1.3", "réponse 1.4"),
-                array("réponse 2.1", "réponse 2.2", "réponse 2.3", "réponse 2.4"),
-                array("réponse 3.1", "réponse 3.2", "réponse 3.3", "réponse 3.4"),
-                array("réponse 4.1", "réponse 4.2", "réponse 4.3", "réponse 4.4"),
-                array("réponse 5.1", "réponse 5.2", "réponse 5.3", "réponse 5.4"),
-                array("réponse 6.1", "réponse 6.2", "réponse 6.3", "réponse 6.4"),
-                array("réponse 7.1", "réponse 7.2", "réponse 7.3", "réponse 7.4"),
-                array("réponse 8.1", "réponse 8.2", "réponse 8.3", "réponse 8.4"),
-                array("réponse 9.1", "réponse 9.2", "réponse 9.3", "réponse 9.4"),
-                array("réponse 10.1", "réponse 10.2", "réponse 10.3", "réponse 10.4"),
-            );
+            $host = 'localhost';
+            $dbname = 'sae401';
+            $username = 'root';
+            $password = '';
 
-
-            for ($i = 1; $i < 11; $i++) {
-                echo '<article><p><strong>' . $i . '-</strong> ' . $question[$i - 1] . '<ul>';
-                for ($j = 0; $j < count($response[$i - 1]); $j++) {
-                    echo ' <li>   <input type="radio" id="question-'.$i.'"
-                    name="question-'.$i.'" value="'.$i.$j.'" required>';
-                    echo '
-                    <label for="question-'.$i.'">'.$response[$i-1][$j].'</label></li>';
-                }
-                echo '</ul></article>';
+            try {
+                $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+            } catch (PDOException $e) {
+                echo "Erreur de connexion à la base de données : " . $e->getMessage();
+                exit();
             }
+
+            $ids = array();
+            // Récupération des questions et réponses
+            $query = "SELECT * FROM questionnaire WHERE id BETWEEN 1 AND 40 GROUP BY id ORDER BY RAND() LIMIT 10";
+            $stmt = $db->query($query);
+            $questions = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $e = 1;
+            foreach ($questions as $question) {
+                echo '<article><p><strong>' . $e . '-</strong> ' . $question['question'] . '<ul>';
+                echo '<li><input type="radio" name="question-' . $question['id'] . '" value="a" required>';
+                echo '<label>' . $question['rep_a'] . '</label></li>';
+                echo '<li><input type="radio" checked name="question-' . $question['id'] . '" value="b" required>';
+                echo '<label>' . $question['rep_b'] . '</label></li>';
+                echo '<li><input type="radio" name="question-' . $question['id'] . '" value="c" required>';
+                echo '<label>' . $question['rep_c'] . '</label></li>';
+                echo '<li><input type="radio" name="question-' . $question['id'] . '" value="d" required>';
+                echo '<label>' . $question['rep_d'] . '</label></li>';
+                echo '</ul></article>';
+                $ids[] = $question['id'];
+                $e++;
+            }
+            print_r($ids);
+            $_SESSION['id'] = $ids;
             ?>
+            <script>
+                console.log(<?php print_r($ids);?>)
+            </script>
             <input type="submit" value="envoyer">
-            </form>
+        </form>
 
     </main>
 
@@ -71,8 +84,7 @@
         <ul>
             <li>look.down@gmail.com</li>
             <li>Copyright 2023 - Tous droits réservés - Look Down</li>
-            <li><i class="fa-brands fa-square-instagram"></i><i class="fa-brands fa-square-twitter"></i><i
-                    class="fa-brands fa-square-facebook"></i></li>
+            <li><i class="fa-brands fa-square-instagram"></i><i class="fa-brands fa-square-twitter"></i><i class="fa-brands fa-square-facebook"></i></li>
         </ul>
     </footer>
     <!--Footer-->
--- page/reponse.php
+++ page/reponse.php
@@ -1,3 +1,26 @@
+<?php
+$score = 9;
+session_start();
+$IDS = $_SESSION['id'];
+
+
+$host = 'localhost';
+$dbname = 'sae401';
+$username = 'root';
+$password = '';
+
+try {
+    $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $query = "SELECT * FROM questionnaire WHERE id IN (" . implode(",", $IDS) . ")";
+    $stmt = $db->prepare($query);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+} catch (PDOException $e) {
+    echo "Erreur de connexion à la base de données : " . $e->getMessage();
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,11 +31,8 @@
     <link rel="stylesheet" href="../media/reset.css">
     <link rel="stylesheet" href="../media/main.css">
     <link rel="stylesheet" href="../media/reponse.css">
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
     <title>Eco responsable</title>
-    <style>
-    </style>
 </head>
 
 <body>
@@ -28,128 +48,26 @@
         </ul>
     </header>
     <!--Header-->
-
     <main>
-
-        <section>
-            <h1>Prêt pour le <strong>defi</strong> ?</h1>
-            <blockquote>Voici quelques actions concrètes que vous pouvez mettre en place pour agir et soutenir la cause
-                :</blockquote>
-        </section>
-
-        <section>
+        <section class="card">
+            <h1>Vous avez eu l'honorable score de : <?= $score; ?>/10</h1>
+            <blockquote>Voici le récapitulatif de chaque questions</blockquote>
+            <br>
             <?php
-
-
-            // Add 6 more items to the array
-            for ($i = 1; $i <= 10; $i++) {
-                $carouselSlides[] = array(
-                    'id' => "carousel__slide$i",
-                    'prev' => "#carousel__slide" . ($i - 1),
-                    'next' => "#carousel__slide" . ($i + 1)
-                );
+            for ($i = 0; $i < 10; $i++) {
+                echo '<ul>';
+                echo '<li><h4><strong>'.$i.'</strong>-' . $result[$i]['question'] . '</h4></li>';
+                echo '<li><h5> Réponse: ' . $result[$i]['rep_a'] . '</h5></li>';
+                echo '</ul>';
             }
-
-            // Encode the array as JSON and save to file
-            $json = json_encode($carouselSlides);
-            file_put_contents('data.json', $json);
             ?>
-            <section class="carousel" aria-label="Gallery">
-                <ol class="carousel__viewport">
-                    <?php foreach ($carouselSlides as $slide): ?>
-                        <li id="<?php echo $slide['id']; ?>" tabindex="0" class="carousel__slide">
-                            <div class="carousel__snapper">
-
-
-                                <?php
-                                if ($slide['id'] == 'carousel__slide1') {
-                                    echo "hey
-                                    
-                                    <canvas id='myChart' style='width: 10%;'></canvas>";
-                                } elseif ($slide['id'] == 'carousel__slide2') {
-                                    echo "salut";
-                                } elseif ($slide['id'] == 'carousel__slide3') {
-                                    echo "ça";
-                                } elseif ($slide['id'] == 'carousel__slide4') {
-                                    echo "slide 4";
-                                } elseif ($slide['id'] == 'carousel__slide5') {
-                                    echo "slide 5";
-                                } elseif ($slide['id'] == 'carousel__slide6') {
-                                    echo "slide 6 ";
-                                } elseif ($slide['id'] == 'carousel__slide7') {
-                                    echo "slide 7 ";
-                                } elseif ($slide['id'] == 'carousel__slide8') {
-                                    echo "slide 8 ";
-                                } elseif ($slide['id'] == 'carousel__slide9') {
-                                    echo "slide 9 ";
-                                } elseif ($slide['id'] == 'carousel__slide10') {
-                                    echo "slide 10 ";
-                                }
-
-                                ?>
-
-
-                            </div>
-                            <a href="<?php echo $slide['prev']; ?>" class="carousel__prev">Go to previous slide</a>
- <script>
-		// génération des données aléatoires
-		var randomData = [];
-		for (var i = 0; i < 4; i++) {
-			randomData.push(Math.floor(Math.random() * 90000) + 10000);
-		}
-
-		// création du chart
-		var ctx = document.getElementById('myChart').getContext('2d');
-		var myChart = new Chart(ctx, {
-		    type: 'doughnut',
-		    data: {
-		        labels: ['A', 'B', 'C', 'D'],
-		        datasets: [{
-		            data: randomData,
-		            backgroundColor: [
-		                'rgba(255, 99, 132, 0.6)',
-		                'rgba(54, 162, 235, 0.6)',
-		                'rgba(255, 206, 86, 0.6)',
-		                'rgba(75, 192, 192, 0.6)'
-		            ]
-		        }]
-		    },
-		    options: {}
-		});
-	</script>
-                            <a href="<?php echo $slide['next']; ?>" class="carousel__next">Go to next slide</a>
-                        </li>
-                    <?php endforeach;
-                    ?>
-                </ol>
-
-                <aside class="carousel__navigation">
-                    <ol class="carousel__navigation-list">
-                        <?php for ($i = 1; $i <= count($carouselSlides); $i++): ?>
-                            <li class="carousel__navigation-item">
-                                <a href="#<?php echo $carouselSlides[$i - 1]['id']; ?>" class="carousel__navigation-button">
-                                    Go to slide <?php echo $i; ?>
-                                </a>
-                            </li>
-                        <?php endfor; ?>
-                    </ol>
-
-
-            </section>
-
         </section>
     </main>
-
-
-
-
-
     <footer>
         <ul>
             <li>look.down@gmail.com</li>
             <li>Copyright 2023 - Tous droits réservés - Look Down</li>
-            <li><i class="fa-brands fa-square-instagram"></i><i class="fa-brands fa-square-twitter"></i><i
-                    class="fa-brands fa-square-facebook"></i></li>
+            <li><i class="fa-brands fa-square-instagram"></i><i class="fa-brands fa-square-twitter"></i><i class="fa-brands fa-square-facebook"></i></li>
         </ul>
     </footer>
     <!--Footer-->
